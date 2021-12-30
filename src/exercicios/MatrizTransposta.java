560bd95c20b11465f09f0a2e7b213c332f668866
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-30 18:05:26 -0300

    01-Adicionando arquivo

---
+++ src/exercicios/MatrizTransposta.java
@@ -0,0 +1,33 @@
+/*Fazer uma função que receba uma matriz e calcule a sua transposta e retorne*/
+/*package exercicios;
+import java.util.Random;
+
+public class MatrizTransposta {
+
+    static int [][] criaMatriz(int N, int M){
+        int [][] matriz = new int[N][M];
+        Random gerador = new Random();
+        for(int i = 0; i < matriz.length; i++){
+            for (int j =0; j < matriz[0].length; j++){
+                matriz[i][j] = gerador.nextInt(100);
+            }
+        }
+        return matriz;
+    }
+
+    static void imprimeMatriz(int [][] matriz){
+      /*  for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    public static void main (String [] args){
+        int [][] matriz = criaMatriz(3, 4);
+        imprimeMatriz(matriz);
+    }
+}
+*/
